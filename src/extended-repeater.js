52a3230eadb5914a5312f6db0eab90e275b39439
RinatMiniy <rinatminiy@gmail.com>
2021-03-22 18:57:09 +0300

    feat: decision carbon and repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  let fin = "",
+  sep = options.separator == undefined ? "+" : options.separator,
+  addsep = options.additionSeparator == undefined ? "|" : options.additionSeparator
+
+  if (options.repeatTimes == undefined) return str+options.addition
   
+  for (let i = 0; i < options.repeatTimes; i++) {
+    fin+=str
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      if (options.additionRepeatTimes - j == 1) {
+        fin += options.addition 
+        continue
+      }
+      fin+= options.addition + addsep
+    }
+    fin+= sep
+  }
+  return fin.slice(0, fin.length - sep.length )
+};
