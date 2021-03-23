6a8a4ae284c6e028224a62d244ceef1e250f891f
RinatMiniy <rinatminiy@gmail.com>
2021-03-23 18:45:11 +0300

    fear: decision hanoi and depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    
+   
+    return recursive(arr)
+    function recursive (arr) {
+      let max = 1,
+      cul = 1
+      for (let i =0; i<arr.length; i++) {
+        if (typeof(arr[i]) == "object") {
+          cul = 1 + recursive(arr[i])
+        }
+        else continue
+      }
+      if (cul>max) max = cul
+      cul = 1
+      return max
+    }
+       
   }
+ 
 };
+
