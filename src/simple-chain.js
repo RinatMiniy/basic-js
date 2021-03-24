f5c9b33adf90a348489c4dd6eb16f8631105f92d
RinatMiniy <rinatminiy@gmail.com>
2021-03-24 18:31:27 +0300

    feat: dicision chain and season

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  "chain": [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      if (value === undefined) value = " "
+      this.chain.push(String(value))
+      return chainMaker
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      if (typeof(position) != "number" || position < 0 || position > this.chain.length){
+          this.chain= []
+          throw new Error()
+      }
+      this.chain.splice(Number(position) - 1, 1)
+      return chainMaker
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      this.chain.reverse()
+      return chainMaker
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      let fin = this.chain.join(" )~~( ")
+      this.chain = []
+      return "( " + fin + " )"
   }
 };
 
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (typeof(date) != "object") {
+    throw new Error()
+  }
+  return null
+  let months = date.getMonth()
+  if (months < 2) return "winter"
+  if (months < 5) return "spring"
+  if (months < 8) return "summer"
+  if (months < 11) return "fall"
+  return "winter"
 };
